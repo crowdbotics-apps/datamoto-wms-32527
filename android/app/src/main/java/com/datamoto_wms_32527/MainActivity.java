aa2a9172f0f7efb6a57c24eda8264e0885ee1405
Crowdbotics <team@crowdbotics.com>
2021-12-22 00:23:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/datamoto_wms_32527/MainActivity.java
@@ -0,0 +1,15 @@
+package com.datamoto_wms_32527;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "datamoto_wms_32527";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "datamoto_wms_32527",
+  url: "https://datamoto_wms_32527.botics.co",
+  api: "https://datamoto_wms_32527.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
